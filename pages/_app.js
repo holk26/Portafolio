86a4d48db8cec1c63874122ef1af64da5e34f8a6
Holk CA <homero97c@hotmail.com>
2023-04-20 11:40:59 -0500

    go

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-//import "bootswatch/dist/zephyr/bootstrap.min.css";
-import "bootswatch/dist/lux/bootstrap.min.css";
+import "bootswatch/dist/zephyr/bootstrap.min.css";
+//import "bootswatch/dist/lux/bootstrap.min.css";
 import "../global.css"
 
 export default function MyApp({ Component, pageProps }) {
